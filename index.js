0fca28b0344502d967b35e05cc50f82d7b76813d
Coco <coconz@live.com>
2015-11-24 17:57:38 +1300

    #3

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ app.use(bodyParser.json())
 
 //make an empty list of ideas
 var coolIdeas = [];
-coolIdeas.push("try wearing a hat on cold days");
-
+var idea = {};
+idea.text = "try wearing a hat on cold days";
+var idea = {};
+idea.text = req.body.idea;
+coolIdeas.push(idea);
 //let a client GET the list of ideas
 app.get('/ideas', function (req, res) {
   res.send(coolIdeas);
